b3bacabd26da536379a37d630a5a48504b3c7132
mayankparihar <mayank.dev.parihar1988gmail.com>
2026-04-01 11:54:49 +0530

    adding authentication handler for jwt

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,7 @@
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+
 namespace WebApi;
 
 public class Program
@@ -9,6 +13,28 @@ public class Program
         // Add services to the container.
 
         builder.Services.AddControllers();
+        
+        // Add Authentication middleware 
+        builder.Services.AddAuthentication()
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidateAudience = true,
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
+                    ),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+        
+       // builder.Services.AddAuthorization();
+        
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
@@ -24,6 +50,7 @@ public class Program
 
         app.UseHttpsRedirection();
 
+        app.UseAuthentication();
         app.UseAuthorization();
 
 
